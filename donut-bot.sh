449182bfbe2adea463012a07e6b72468f77afb40
Matthew <matthew.gimbl@gmail.com>
2023-11-04 21:57:28 -0500

    added check to donut-bot.sh to ensure litestream (database replication) is running before starting donut-bot

--- donut-bot.sh
+++ donut-bot.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+LS_STATUS=$( systemctl is-active litestream )
+if [ '$LS_STATUS' != 'active' ]; then
+  echo "litestream replication not running, starting ..."
+  systemctl start litestream
+fi
+
+LS_STATUS=$( systemctl is-active litestream )
+if [ '$LS_STATUS' != 'active' ]; then
+  echo "litestream replication failed to start, exiting ..."
+  exit 4
+fi
+
 nohup python3.11 main.py > nohup.log 2>&1 &
 echo $! > pid.txt
