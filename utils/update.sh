087a7b694c6010da330a9a350fbe8b17b13d165f
Matthew <matthew.gimbl@gmail.com>
2023-11-06 22:01:15 -0600

    restructured utils directory

--- utils/update.sh
+++ utils/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=$( cat pid.txt )
+PID=$( cat ../pid.txt )
 
 if ps -p $PID > /dev/null; then
    echo "donut-bot is running, stopping process..."
@@ -17,28 +17,19 @@ else
   echo "litestream replication not running, ok ..."
 fi
 
-echo "creating backup of donut-bot.db..."
-if [ -e database/donut-bot.db ]; then
-  TIMESTAMP=$( date +%Y_%m_%d_%H_%M_%S )
-  BACKUP_PATH=database/backups/$TIMESTAMP
-
-  mkdir -p $BACKUP_PATH
-  cp database/donut-bot.db $BACKUP_PATH
-else
-  echo "donut-bot.db does not exist, will create from restore..."
-fi
+./backup_db.sh
 
 echo "pulling down new application code..."
 #git reset --hard
 git pull
 
-if [ -e database/donut-bot.db ]; then
+if [ -e ../database/donut-bot.db ]; then
   #echo removing existing donut-bot.db
   rm database/donut-bot.db
 fi
 
 #echo "restoring donut-bot from replicated database..."
-cd database
+cd ../database
 litestream restore -o donut-bot.db donut-bot.db
 cd ..
 
@@ -47,6 +38,6 @@ sudo systemctl start litestream
 
 #echo "chmoding scripts to executable..."
 chmod +x donut-bot.sh
-chmod +x update.sh
+chmod +x utils/update.sh
 
 #echo "completed successfully"
