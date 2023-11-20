be0813a4ab7c333e87be0aed0b4ae60f683b464f
Matthew <matthew.gimbl@gmail.com>
2023-11-20 08:11:34 -0600

    removed un-needed call to get users.json

--- ad_hoc/associate_unregistered_tips.py
+++ ad_hoc/associate_unregistered_tips.py
@@ -1,13 +1,9 @@
-import json
 import os.path
-import urllib.request
 import sqlite3
 
 from datetime import datetime
 
 if __name__ == '__main__':
-    user_json = json.load(urllib.request.urlopen("https://ethtrader.github.io/donut.distribution/users.json"))
-
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, "../database/donut-bot.db")
     db_path = os.path.normpath(db_path)
