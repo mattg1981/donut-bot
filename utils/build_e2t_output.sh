16830c0ed664487d7d6a90813aead83e7666efc3
Matthew <matthew.gimbl@gmail.com>
2023-11-27 09:33:28 -0600

    create e2t output shell script

--- utils/build_e2t_output.sh
+++ utils/build_e2t_output.sh
@@ -4,19 +4,19 @@
 #  outputs tip information, commits the file and pushes
 #
 
-echo "begin building tips output file..."
+echo "begin building e2t output file..."
 TIMESTAMP_DAY=$( date +%Y/%m/%d )
 TIMESTAMP=$( date +%H:%M )
 
 echo "associating any unregistered tips"
 python3.11 ../ad_hoc/associate_unregistered_tips.py
 
-echo "building tip output file"
-python3.11 ../ad_hoc/build_tips_output.py
+echo "applying tips and funded accounts"
+python3.11 ../ad_hoc/calculate_distribution_e2t.py
 
 cd ../out
-cp -fr ./tips_round_* ../../donut-bot-output/tips/
-cd ../../donut-bot-output/tips/
+cp -fr ../round_* ../../donut-bot-output/csv/
+cd ../../donut-bot-output/csv/
 
 echo "git pull"
 git pull
@@ -25,7 +25,7 @@ echo "git add (in case a new file is available"
 git add . --force
 
 echo "git commit..."
-git commit -m "updated tips output json ${TIMESTAMP_DAY} ${TIMESTAMP}" *
+git commit -m "applied e2t tips ${TIMESTAMP_DAY} ${TIMESTAMP}" *
 
 echo "git push..."
 git push
