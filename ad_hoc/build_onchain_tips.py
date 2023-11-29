d76eae4e292440714c81f20d2305217e5267cf49
Matthew <matthew.gimbl@gmail.com>
2023-11-29 10:55:12 -0600

    initial build_ban_list.py

--- ad_hoc/build_onchain_tips.py
+++ ad_hoc/build_onchain_tips.py
@@ -7,6 +7,7 @@ from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
 from dotenv import load_dotenv
+from web3 import Web3
 
 if __name__ == '__main__':
     # load environment variables
@@ -35,18 +36,59 @@ if __name__ == '__main__':
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
+    with open(os.path.normpath("../contracts/tipping_contract_abi.json"), 'r') as f:
+        tip_abi = json.load(f)
+
+    w3_connected = False
+    tipping_contract = None
+    gno_w3 = None
+
+    for public_node in config["gno_public_nodes"]:
+        try:
+            logger.info(f"  trying GNO node {public_node}")
+            gno_w3 = Web3(Web3.HTTPProvider(public_node))
+            if gno_w3.is_connected():
+                w3_connected = True
+                tipping_contract = gno_w3.eth.contract(address=Web3.to_checksum_address(config['tipping_contract_address']), abi=tip_abi)
+                break
+        except Exception as e:
+            logger.error(f"  {e}")
+
+    if not w3_connected:
+        logger.error(f"  exhausted all public nodes, aborting...")
+
     with sqlite3.connect(db_path) as db:
+        # build the table and index if it is the first run of this application
+        build_table_and_index = """
+           CREATE TABLE IF NOT EXISTS onchain_tip (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                from_address NVARCHAR2 NOT NULL COLLATE NOCASE,
+                to_address NVARCHAR2 NOT NULL COLLATE NOCASE,
+                tx_hash NVARCHAR2 NOT NULL COLLATE NOCASE,
+                block BIGINT,
+                amount DECIMAL(10, 5) NOT NULL,
+                token NVARCHAR2 NOT NULL,
+                content_id NVARCHAR2,
+                timestamp DATETIME NOT NULL,
+                created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+
+            CREATE UNIQUE INDEX IF NOT EXISTS
+            onchain_tip_tx_hash_idx on onchain_tip(tx_hash);
+        """
+
         db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
         cursor = db.cursor()
+        cursor.executescript(build_table_and_index)
         cursor.execute("select max(block) 'block' from onchain_tip")
         max_block = cursor.fetchone()['block']
 
     if not max_block:
-        max_block = 0 # 30400000
+        max_block = 0
 
     #  handle edge case where the last update may have returned > 10,000 records.
-    #  If we remove and re-add the last block, we can ensure that we will grab all tips in the block.
-    #  (unless a single block had more than 10,000 tips in it - very unlikely)
+    #  we remove all records from the lastest block in the database and then start
+    #  grabbing new tips from that block onward.
     if max_block > 0:
         with sqlite3.connect(db_path) as db:
             db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
@@ -81,15 +123,21 @@ if __name__ == '__main__':
     # keep only 'tip' methods
     results = [x for x in results if x['methodId'] == '0xeb4e2e5d']
 
-    # map the results to confirm to the sql query below
     tips = []
     for result in results:
-        amount = 0
-        content_id = ''
-        token_contract_address = ''
-        to_address = ''
-
         # get to_address, token, content_id and amount from the input
+        input = tipping_contract.decode_function_input(result['input'])
+        to_address = input[1]['_to']
+        token_contract_address = input[1]['_token']
+        amount = float(int(input[1]['_amount']) / float(1e18))
+
+        content_id_bytes = input[1]["_contentId"]
+        content_id = gno_w3.to_text(content_id_bytes.hex()).replace("\x00", "")
+
+        # special case when content_id is not properly written
+        if "t3_" not in content_id and "t1_" not in content_id:
+            logger.warning(f" direct tip to multisig, tx_hash {result['hash']}")
+            content_id = None
 
         # todo make this a more dynamic lookup
         if token_contract_address.lower() == '0x524b969793a64a602342d89bc2789d43a016b13a':
@@ -97,11 +145,13 @@ if __name__ == '__main__':
         else:
             token = token_contract_address
 
-        # keep just the fields we want
+        # map the results to conform to the sql query below keeping just the fields we want
         tips.append((
             result['from'], to_address, result['hash'], result['blockNumber'], amount, token, content_id,
             datetime.fromtimestamp(int(result['timeStamp']))
         ))
+
+    logger.info("saving tips to database")
 
     # save the tips to db
     with sqlite3.connect(db_path) as db:
@@ -114,3 +164,5 @@ if __name__ == '__main__':
 
         cursor = db.cursor()
         cursor.executemany(sql, tips)
+
+    logger.info("complete")
---
+++ ad_hoc/export_tips_for_distribution.py
@@ -0,0 +1,69 @@
+import csv
+import json
+import os.path
+import urllib.request
+import sqlite3
+
+from datetime import datetime
+
+if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "../database/donut-bot.db")
+    db_path = os.path.normpath(db_path)
+
+    DISTRIBUTION_ROUND = 130
+    HARDCODED_BLOCK_FOR_OFFCHAIN = 30744221
+
+    export_query = """
+        SELECT 
+           u1.username 'from_user'
+          ,t.from_address
+          ,u2.username 'to_user'
+          ,t.to_address
+          ,t.tx_hash
+          ,t.block
+          ,t.amount
+          ,t.[timestamp]
+          ,t.content_id
+        FROM onchain_tip t
+         left join users u1 on t.from_address = u1.address collate nocase
+         left join users u2 on t.to_address = u2.address collate nocase
+        
+        UNION
+        
+        SELECT 
+          u1.username 'from_user'
+          , e.from_address
+          , e.to_user
+          , u2.address 'to_address'
+          , 'offchain:' || e.id 'tx_hash'
+          , ? 'block'
+          , e.amount
+          , e.created_date 'timestamp'
+          , e.parent_content_id 'content_id'
+        from earn2tip e
+          left join users u1 on e.from_address = u1.address collate nocase
+          left join users u2 on e.to_user = u2.username collate nocase
+        where e.created_date between 
+          (select from_date from distribution_rounds where distribution_round = ?) and (select to_date from distribution_rounds where distribution_round = ?)
+        
+        order by timestamp desc
+    """
+
+    with sqlite3.connect(db_path) as db:
+        db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        cursor = db.cursor()
+        cursor.execute(export_query, [HARDCODED_BLOCK_FOR_OFFCHAIN, DISTRIBUTION_ROUND, DISTRIBUTION_ROUND])
+        tips = cursor.fetchall()
+
+    out_file = f"../out/export_tips_for_distribution_round_{DISTRIBUTION_ROUND}.csv"
+
+    if os.path.exists(out_file):
+        os.remove(out_file)
+
+    # write new csv
+    with open(out_file, 'w') as output_file:
+        writer = csv.DictWriter(output_file, tips[0].keys(), extrasaction='ignore')
+        writer.writeheader()
+        writer.writerows(tips)
+
