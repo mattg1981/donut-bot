21e434702c7cfd7ee40438bbfbdd27db96895bf3
Matthew <matthew.gimbl@gmail.com>
2023-12-01 15:49:13 -0600

    build onchain tips file

---
+++ ad_hoc/export_onchain_tips.py
@@ -0,0 +1,44 @@
+import csv
+import os
+import sqlite3
+
+if __name__ == '__main__':
+    # locate database
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "../database/donut-bot.db")
+    db_path = os.path.normpath(db_path)
+
+    with sqlite3.connect(db_path) as db:
+        query = """
+        SELECT
+           u1.username from_user,
+           from_address,
+           u2.username to_user,
+           to_address,
+           tx_hash,
+           block,
+           amount,
+           token,
+           t.content_id,
+           timestamp
+          FROM onchain_tip t
+            left join users u1 on t.from_address = u1.address
+            left join users u2 on t.to_address = u2.address
+          ORDER BY block desc
+
+        """
+        db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        cursor = db.cursor()
+        cursor.execute(query)
+        tips = cursor.fetchall()
+
+    out_file = "../out/onchain_tips.csv"
+
+    if os.path.exists(out_file):
+        os.remove(out_file)
+
+    # write new csv
+    with open(out_file, 'w') as output_file:
+        writer = csv.DictWriter(output_file, tips[0].keys(), extrasaction='ignore')
+        writer.writeheader()
+        writer.writerows(tips)
--- utils/build_tips_output.sh
+++ utils/build_tips_output.sh
@@ -11,18 +11,23 @@ TIMESTAMP=$( date +%H:%M )
 echo "associating any unregistered tips"
 python3.11 ../ad_hoc/associate_unregistered_tips.py
 
-echo "building tip output file"
+echo "building off-chain tip output file"
 python3.11 ../ad_hoc/build_tips_output.py
+
+echo "building on-chain tip output file"
+python3.11 ../ad_hoc/export_onchain_tips.py
 
 cd ../out
 cp -fr ./tips_round_* ../../donut-bot-output/offchain_tips/
-cd ../../donut-bot-output/offchain_tips/
+cp -fr ./onchain_tips.csv ../../donut-bot-output/onchain_tips
+
+cd ../../donut-bot-output/
 
 echo "git pull"
 git pull
 
 echo "git add (in case a new file is available"
-git add . --force
+git add --all --force
 
 echo "git commit..."
 git commit -m "updated tips output json ${TIMESTAMP_DAY} ${TIMESTAMP}" *
