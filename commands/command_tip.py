21e0e4125dad1ced1686019b01613f9d44b1f74d
Matthew <matthew.gimbl@gmail.com>
2023-12-08 10:18:06 -0600

    match previous log statements so the logs have the same behavior

--- commands/command_tip.py
+++ commands/command_tip.py
@@ -299,6 +299,7 @@ class TipCommand(Command):
         if not valid_tips:
             self.leave_comment_reply(comment, reply)
         elif database.process_earn2tips(valid_tips, Path(__file__).stem):
+            self.logger("success...")
             self.leave_comment_reply(comment, reply, False)
         else:
             self.leave_comment_reply(comment, f"❌ Sorry u/{comment.author.name}, I was unable to process your "
