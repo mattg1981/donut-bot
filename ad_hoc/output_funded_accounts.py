d38122181438b6925562e8fbca5ed4cb863e32f2
Matthew <matthew.gimbl@gmail.com>
2023-12-11 09:32:24 -0600

    export funded account information for the round

---
+++ ad_hoc/output_funded_accounts.py
@@ -0,0 +1,72 @@
+import json
+import os.path
+import urllib.request
+import sqlite3
+
+from datetime import datetime
+
+if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "../database/donut-bot.db")
+    db_path = os.path.normpath(db_path)
+
+    rounds_query = """
+        select distribution_round from distribution_rounds
+        where DATE() between from_date and to_date;
+    """
+
+    tips_query = """
+    SELECT 
+       from_user user,
+       amount,
+       token,
+       block_number,
+       tx_hash,
+       tx_timestamp,
+       processed_at,
+       created_at
+  FROM funded_account
+    where
+      created_at between (
+        select
+          from_date
+        from
+          distribution_rounds
+        where
+          distribution_round = ?
+      ) and (
+        select
+          to_date
+        from
+          distribution_rounds
+        where
+          distribution_round = ?
+      );
+    """
+
+    with sqlite3.connect(db_path) as db:
+        db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        cursor = db.cursor()
+        cursor.execute(rounds_query)
+        round_result = cursor.fetchone()
+
+    # build tips for last round and current round
+    r_min = int(round_result["distribution_round"]) - 1
+    r_max = int(round_result["distribution_round"])
+
+    for i in range(r_min, r_max):
+        with sqlite3.connect(db_path) as db:
+            db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+            cursor.execute(tips_query, [i, i])
+            tips = cursor.fetchall()
+
+        if not tips:
+            break
+
+        out_file = f"../out/funded_round_{i}.json"
+
+        if os.path.exists(out_file):
+            os.remove(out_file)
+
+        with open(out_file, 'w') as f:
+            json.dump(tips, f, indent=4)
---
+++ utils/output_funded_accounts.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#
+#  outputs tip information, commits the file and pushes
+#
+
+echo "begin building funded accounts output file..."
+TIMESTAMP_DAY=$( date +%Y/%m/%d )
+TIMESTAMP=$( date +%H:%M )
+
+echo "building off-chain tip output file"
+python3.11 ../ad_hoc/output_funded_accounts.py
+
+cd ../out
+cp -fr ./funded_round_* ../../donut-bot-output/funded_accounts/
+
+cd ../../donut-bot-output/
+
+echo "git pull"
+git pull
+
+echo "git add (in case a new file is available"
+git add --all --force
+
+echo "git commit..."
+git commit -m "updated funded accounts output json ${TIMESTAMP_DAY} ${TIMESTAMP}" *
+
+echo "git push..."
+git push
+
+echo "completed successfully"
