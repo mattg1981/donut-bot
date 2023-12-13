b184b176766acdb1d7f7778c7cc593d7f21227cb
Matthew <matthew.gimbl@gmail.com>
2023-12-13 10:11:03 -0600

    export ban information

---
+++ ad_hoc/export_bans.py
@@ -0,0 +1,79 @@
+import json
+import os.path
+import urllib.request
+import sqlite3
+
+from datetime import datetime
+
+if __name__ == '__main__':
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "../database/donut-bot.db")
+    db_path = os.path.normpath(db_path)
+
+    rounds_query = """
+        select distribution_round from distribution_rounds
+        where DATE() between from_date and to_date;
+    """
+
+    temp_ban_query = """
+    SELECT 
+       username,
+       ban_date,
+       community
+    FROM bans
+        where permanent = 0 
+        and ban_date between (select from_date from distribution_rounds where distribution_round = ?) 
+          and (select to_date from distribution_rounds where distribution_round = ?)    
+    """
+
+    with sqlite3.connect(db_path) as db:
+        db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        cursor = db.cursor()
+        cursor.execute(rounds_query)
+        round_result = cursor.fetchone()
+
+    # build tips for last round and current round
+    round_min = int(round_result["distribution_round"]) - 1
+    round_max = int(round_result["distribution_round"])
+
+    for i in range(round_min, round_max + 1):
+        with sqlite3.connect(db_path) as db:
+            db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+            cursor.execute(temp_ban_query, [i, i])
+            temp_bans = cursor.fetchall()
+
+        if not temp_bans:
+            break
+
+        out_file = f"../out/temp_bans_round_{i}.json"
+
+        if os.path.exists(out_file):
+            os.remove(out_file)
+
+        with open(out_file, 'w') as f:
+            json.dump(temp_bans, f, indent=4)
+
+    perm_ban_query = """
+        SELECT 
+           username,
+           ban_date,
+           community
+        FROM bans
+            where permanent = 1 
+        ORDER by 
+            ban_date desc    
+        """
+
+    with sqlite3.connect(db_path) as db:
+        db.row_factory = lambda c, r: dict(zip([col[0] for col in c.description], r))
+        cursor = db.cursor()
+        cursor.execute(perm_ban_query)
+        perm_bans = cursor.fetchall()
+
+    perm_bans_out_file = f"../out/perm_bans.json"
+
+    if os.path.exists(perm_bans_out_file):
+        os.remove(perm_bans_out_file)
+
+    with open(perm_bans_out_file, 'w') as f:
+        json.dump(perm_bans, f, indent=4)
---
+++ utils/export_bans.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+#  outputs tip information, commits the file and pushes
+#
+
+echo "begin exporting bans ..."
+TIMESTAMP_DAY=$( date +%Y/%m/%d )
+TIMESTAMP=$( date +%H:%M )
+
+python3.11 ../ad_hoc/export_bans.py
+
+echo "exported bans ..."
+echo "begin to add bans to repository"
+
+cd ../out
+cp -fr ./perm_bans* ../../donut-bot-output/bans/
+cp -fr ./temp_bans_round_* ../../donut-bot-output/bans/
+
+cd ../../donut-bot-output/bans/
+
+echo "git pull"
+git pull
+
+echo "git add"
+git add --all --force
+
+echo "git commit..."
+git commit -m "updated moderators ${TIMESTAMP_DAY} ${TIMESTAMP}" *
+
+echo "git push..."
+git push
+
+echo "completed successfully"
