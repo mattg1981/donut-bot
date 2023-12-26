c8a420c638357449f90f746d1001db0119c44376
Matthew <matthew.gimbl@gmail.com>
2023-12-26 17:36:51 -0600

    terminate statements with ;

--- ad_hoc/export_bans.py
+++ ad_hoc/export_bans.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     FROM bans
         where permanent = 0 and is_overturned = 0 
         and ban_date between (select from_date from distribution_rounds where distribution_round = ?) 
-          and (select to_date from distribution_rounds where distribution_round = ?)    
+          and (select to_date from distribution_rounds where distribution_round = ?);    
     """
 
     with sqlite3.connect(db_path) as db:
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         FROM bans
             where permanent = 1 and is_overturned = 0 
         ORDER by 
-            ban_date desc    
+            ban_date desc;    
         """
 
     with sqlite3.connect(db_path) as db:
