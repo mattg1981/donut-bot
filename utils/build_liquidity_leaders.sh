0d7d4671e4898bfd247c695ba977c14078b74809
Matthew Gimbl <matthew.gimbl@gmail.com>
2024-04-17 14:12:46 -0500

    fix liquidity leaders

--- utils/build_liquidity_leaders.sh
+++ utils/build_liquidity_leaders.sh
@@ -5,7 +5,7 @@ TIMESTAMP_DAY=$( date +%Y/%m/%d )
 TIMESTAMP=$( date +%H:%M )
 
 python3.11 ../ad_hoc/build_liquidity_leaders.py
-cp -fr ../temp/liquidity_leaders ../../donut-bot-output/liquidity/
+cp -fr ../temp/liquidity_leaders.json ../../donut-bot-output/liquidity/
 cd ../../donut-bot-output/liquidity/
 
 echo "git pull"
